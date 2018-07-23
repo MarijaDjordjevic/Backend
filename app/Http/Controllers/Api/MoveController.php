f3450c2091b22d450b9254e6c1b8087be16acc01
Milos Mosic <milos.mosic@quantox.com>
2018-07-23 12:52:46 +0200

    Added method for checking winner

--- app/Http/Controllers/Api/MoveController.php
+++ app/Http/Controllers/Api/MoveController.php
@@ -15,6 +15,18 @@ class MoveController extends Controller
 {
     public function createMove($game_id, $position)
     {
+        $move = new Move();
+        $player_positions = $this->getPlayerPositions($game_id);
+
+        if ($move->checkCombination($player_positions)) {
+            return 'Game Over';
+        }
+
+        $all_positions = $this->getAllPositions($game_id);
+        if ($all_positions == 9) {
+            return 'Draw';
+        }
+
         $playerX = Game::where('id', $game_id)->first()->player_x;
         $playerO = Game::where('id', $game_id)->first()->player_o;
         if (auth()->user()->id == $playerX) {
@@ -30,5 +42,18 @@ class MoveController extends Controller
         $move->field_type = $field_type;
         $move->save();
         return new MoveResource($move);
+    }
+
+    public function getPlayerPositions($game_id)
+    {
+        $positions = Move::where('game_id', $game_id)
+            ->where('player_id', auth()->user()->id)
+            ->pluck('position')->toArray();
+        return $positions;
+    }
+
+    public function getAllPositions($game_id)
+    {
+        return Move::where('game_id', $game_id)->count();
     }
 }
--- app/Model/Move.php
+++ app/Model/Move.php
@@ -4,7 +4,38 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Move
+ * @package App\Model
+ */
 class Move extends Model
 {
-    //
+    /**
+     * @var array
+     */
+    public $combinations = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7],
+    ];
+
+    /**
+     * @param array $positions
+     * @return bool
+     */
+    public function checkCombination($positions)
+    {
+        foreach ($this->combinations as $combination) {
+            $result = array_intersect($positions, $combination);
+            if (count($result) == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
